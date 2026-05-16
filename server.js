93e16b1c1e9803a95f16a771bfaeecd99fbb84aa
Marissa Goldberg <mar15@users.noreply.github.com>
2026-05-16 12:28:11 -0600

    Serve social preview as PNG

--- server.js
+++ server.js
@@ -215,6 +215,7 @@ const mimeTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
   ".svg": "image/svg+xml",
 };
 
